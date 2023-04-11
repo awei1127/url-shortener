4c23148f5c9acc8a1d368d7806c39c9594d8cba1
Wei <awei1127@hotmail.com>
2023-04-12 01:04:57 +0800

    feat: add routes design

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 const generateUrl = require('./generate_url')
 
+
 const app = express()
 const port = 3000
 
@@ -17,12 +18,30 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 // 以下路由
-app.get('/', (req, res) => {
+// 首頁
+app.get('/main/index', (req, res) => {
   generateUrl()
   res.render('index')
 })
 
+// 縮址請求
+app.post('/main/index', (req, res) => {
+  // 拿到使用者輸入的網址
+  // 搜尋資料庫 如果已存在就取出隨機碼 如果不存在就產生一組隨機碼且與輸入的網址一起存入資料庫
+  // 把使用者輸入的網址跟隨機碼傳入index頁面渲染
+  generateUrl()
+  res.render('index')
+})
+
+
+app.get('/:randomString', (req, res) => {
+  // 去資料庫找資料 如果有就直接轉走 如果沒有就顯示沒找到的頁面
+  generateUrl()
+  res.render('notfound')
+})
+
+
 // 啟動並監聽伺服器
 app.listen(port, () => {
-  console.log(`App is litening on http://localhost:${port}`)
+  console.log(`App is litening on http://localhost:${port}/main/index`)
 })
